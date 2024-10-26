a59eadd5199edd2ed1f9d32f3785b185cdfee1c0
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-10-26 22:40:18 +0530

    Bus search

--- controllers/bus-controller.js
+++ controllers/bus-controller.js
@@ -27,3 +27,23 @@ exports.deleteBus = (req, res) => {
     .then(() => res.status(200).json({ message: "success" }))
     .catch((err) => res.status(500).json({ message: "error", error: err }));
 };
+
+exports.searchBus = (req, res) => {
+  const { departure, arrival } = req.body;
+
+  BusModel.find({
+    departure: departure,
+    arrival: arrival,
+  })
+    .then((buses) => {
+      if (buses.length > 0) {
+        res.json(buses);
+      } else {
+        res.status(404).json("No buses found for the specified locations");
+      }
+    })
+    .catch((error) => {
+      console.error("Error retrieving buses:", error);
+      res.status(500).json("An error occurred while searching for buses");
+    });
+};
--- routes/bus.js
+++ routes/bus.js
@@ -4,6 +4,7 @@ const addBus = require("../controllers/bus-controller").addBus;
 const getAllBus = require("../controllers/bus-controller").getAllBus;
 const updateBus = require("../controllers/bus-controller").updateBus;
 const deleteBus = require("../controllers/bus-controller").deleteBus;
+const searchBus = require("../controllers/bus-controller").searchBus;
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.post("/", addBus);
 router.get("/all", getAllBus);
 router.put("/update/:id", updateBus);
 router.delete("/delete/:id", deleteBus);
+router.post("/search", searchBus);
 
 module.exports = router;
